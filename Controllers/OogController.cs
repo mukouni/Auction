257342fe4efb3d927d8ea719f514b23d028da859
kangyuan <dmzsz@sina.com>
2019-04-26 09:28:56 +0800

    add ogg errlog

--- Controllers/OogController.cs
+++ Controllers/OogController.cs
@@ -39,6 +39,7 @@ namespace Auctions.Controllers
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex.Message);
                 return Ok("Error");
             }
             // return Ok("Success");
